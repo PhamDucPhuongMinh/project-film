32c982fd8be243dddc72a6ee90f06e63a25fe68d
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-09-08 21:06:58 +0700

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <p>© 2022 Michu All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
